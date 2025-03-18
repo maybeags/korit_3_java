47e53c4ed560e4f4747afc795b5103762dd52379
dongyoon7212 <dongyoon7212@naver.com>
2025-03-18 14:41:24 +0900

    feat: 20250318 first commit

---
+++ src/main/java/ch09_classes/ConstructorMain.java
@@ -0,0 +1,13 @@
+package ch09_classes;
+
+public class ConstructorMain {
+    public static void main(String[] args) {
+        // 기본 생성자를 통한 객체 생성
+        Constructor constructor1 = new Constructor();
+
+
+        // 매개변수 생성자를 통한 객체 생성
+        Constructor constructor2 = new Constructor(20);
+        System.out.println(constructor2.num);
+    }
+}
