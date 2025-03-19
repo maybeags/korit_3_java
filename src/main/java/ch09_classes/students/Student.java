6d932ade27a9fa3be0275474062cb7c10bebb082
maybeags <suren0709@gmail.com>
2025-03-19 11:05:31 +0900

    feat : 20250319 student task

--- src/main/java/ch09_classes/students/Student.java
+++ src/main/java/ch09_classes/students/Student.java
@@ -49,4 +49,42 @@ package ch09_classes.students;
 
  */
 public class Student {
+    // 필드 선언
+    int studentCode;
+    String name;
+    double score;
+
+    // 생성자들 정의
+    Student() {
+        System.out.println("기본 생성자로 객체가 생성되었습니다.");
+    }
+
+    Student(int studentCode) {
+        this.studentCode = studentCode;
+        System.out.println("int 매개변수 생성자로 객체가 생성되었습니다.");
+    }
+
+    Student(String name) {
+        this.name = name;
+        System.out.println("String 매개변수 생성자로 객체가 생성되었습니다.");
+    }
+
+    Student(int studentCode, String name) {
+        this.studentCode = studentCode;
+        this.name = name;
+        System.out.println("int, String 매개변수 생성자로 객체가 생성되었습니다.");
+    }
+
+    Student(int studentCode, String name, double score) {
+        this.studentCode = studentCode;
+        this.name = name;
+        this.score = score;
+        System.out.println("int, String, double 매개변수 생성자로 객체가 생성되었습니다.");
+    }
+
+    // showInfo() -> call1() -> {}내부에 sout
+    void showInfo() {
+        System.out.println("\n" + name + " 학생의 정보입니다.\n학번 : " + studentCode);
+        System.out.println("이름 : " + name + "\n점수 : " + score);
+    }
 }
