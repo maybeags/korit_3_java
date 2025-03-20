1b326fbbb1306927d692a24b92fe5c36a6a89f2d
maybeags <suren0709@gmail.com>
2025-03-20 10:50:25 +0900

    feat: 20250320 bmi calc making making

--- src/main/java/org/example/BmiCalc.java
+++ src/main/java/org/example/BmiCalc.java
@@ -1,40 +1,41 @@
 package org.example;
-
-import java.util.Scanner;
-
 /*
-    BMI 계산기를 작성하시오.
+    이하의 코드에 필요한 로직을 작성하고,
+    BmiCalcMain에 적용하여 -> 여기에 객체도 생성해야하고,
+                         -> getBmi라는 메서드도 호출
+    동일한 결과값이 나오도록 작성하시오.
 
-    1. 사용자에게 키(cm)와 몸무게(kg)를 입력 받으시오.
-    2. 수학적 연산을 통해서 BMI 지수를 산출하시오.
-        BMI 지수 = 몸무게(kg) / (키(m)의 제곱)
-    3.  18.5 미만인 경우 저체중
-        23 미만은 정상 체중
-        25 미만은 과체중
-        25 이상에 해당하는 조건문을 작성하고
-    4. 실행 예
-        키(cm)를 입력하세요. >>> 172
-        몸무게(kg)를 입력하세요. >>> 70
-        당신의 BMI 지수는 23.432314이며, 과체중입니다.
+    이 클래스에 getBmiResult 메서드를 call1()으로 정의하여
+    bmiCalc.getBmiResult(); 를 호출 했을 때
+    실행 예
+    키(cm)를 입력하세요 >>> 172
+    몸무게(kg)를 입력하세요 >>> 70
+    당신의 BMI 지수는 23.6659310이며 과체중입니다.
 
-        와 같이 출력될 수 있도록 작성하시오.
+    가 출력되도록 하시오.
  */
 public class BmiCalc {
-    public static void main(String[] args) {
-        // 1. Scanner import
-        Scanner scanner = new Scanner(System.in);
+    public double getBmi(double height, double weight) {
+        double bmi = 0; // 지역 변수
+        height = height * 0.01;
+        bmi = weight / (height*height);
 
-        // 2. 사용할 변수 목록 선언 및 초기화
-        // 생각해볼 점 : 테스트할 때 마다 값을 입력하기 귀찮으면 어떻게 하는게 좋을까?
-        double height = 0;
-        double weight = 0;
+        return bmi;
+    }
 
-        double bmi = 0;
-        System.out.print("키(cm)를 입력하세요 >>> ");
-        height = 172;
-        System.out.print("몸무게(kg)를 입력하세요 >>> ");
-        weight = 70;
+    public String getResult(double bmi) {
+        String status = "";
 
+        if(bmi < 18.5) {
+            status = "저체중";
+        } else if (bmi < 23) {
+            status = "정상 체중";
+        } else if (bmi < 25) {
+            status = "과체중";
+        } else {
+            status = "비만";
+        }
 
+        return status;
     }
 }
