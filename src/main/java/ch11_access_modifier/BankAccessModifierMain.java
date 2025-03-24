019a540bb9773aafa50d21fd70ef71b26c982c74
maybeags <suren0709@gmail.com>
2025-03-24 15:29:49 +0900

    feat: bank access modifier main test

---
+++ src/main/java/ch11_access_modifier/BankAccessModifierMain.java
@@ -0,0 +1,27 @@
+package ch11_access_modifier;
+
+import java.util.Scanner;
+
+public class BankAccessModifierMain {
+    public static void main(String[] args) {
+        // 테스트용 객체 생성
+//        Bank bank1 = new Bank(1234567, "안근수",
+//                10000, 1234);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int pin = 0;
+//
+//
+//
+//        bank1.showInfo();
+//        bank1.deposit(1000, 3214);  // 입금하는데 비밀번호 틀리면
+//        System.out.println(bank1.getBalance()); // 입금되지 않았는지 확인
+//        bank1.deposit(-10, 1234); // 입금하는데 음수값
+//        System.out.println(bank1.getBalance()); // 입금되지 않았는지 확인
+//        bank1.deposit(2000, 1234);
+//        System.out.print("비밀번호를 입력하세요 >>> ");
+//        pin = scanner.nextInt();
+//        bank1.deposit(3000, pin);
+        // pin에 만약에 비밀번호를 맞게 혹은 틀리게 하는 방식으로 테스트 가능
+    }
+}
