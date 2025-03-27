49939dd0d7b8d561d7482f710fd9f72841246398
maybeags <suren0709@gmail.com>
2025-03-27 10:17:07 +0900

    uploaded: modified guideline

--- src/main/java/ch13_abstraction/notification/NotificationSystem.java
+++ src/main/java/ch13_abstraction/notification/NotificationSystem.java
@@ -7,10 +7,22 @@ package ch13_abstraction.notification;
         2) String recipient : 알람 수신자
         3) String message : 알람 메시지
         4) int priority : 알람 우선순위(1-5 범위)
+        5) AllArgsConstructor로 생성자 생성
+
 
     2. 다음과 같은 메서드를 가집니다.
         void pirntNotificationInfo() : 알람 정보 출력
+            실행 예
+            [ 알림 정보 ]
+            From : admin@test.com
+            To : user@example.com
+            Message : 가입을 축하합니다!
+            Priority : 2
+
         void updateMessage(String newMessage) : 메시지 수정
+            실행 예
+            updateMessage("가입 승인"); 으로 호출했을 때
+            '가입 대기'가 '가입 승인'으로 변경되었습니다!
         priority 관련 setter / getter
             setPriority에서 주의할 점 :
                 1 ~5의 범위를 가집니다. 내부에 로직을 작성할 필요가 있겠죠
