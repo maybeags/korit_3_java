90ba2f4c90517dd11a63bfdf24c043849508b867
maybeags <suren0709@gmail.com>
2025-03-27 12:49:53 +0900

    feat: 20250327 interface lesson

---
+++ src/main/java/ch13_abstraction/interfaces/Main.java
@@ -0,0 +1,7 @@
+package ch13_abstraction.interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        TvRemoteController tvRemoteController = new TvRemoteController();
+    }
+}
