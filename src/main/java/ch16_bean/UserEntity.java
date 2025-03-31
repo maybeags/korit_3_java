afa326eb1ea59043fb0284d76311bbd809cb4032
maybeags <suren0709@gmail.com>
2025-03-31 10:51:57 +0900

    feat: 20250331 lombok installed

--- src/main/java/ch16_bean/UserEntity.java
+++ src/main/java/ch16_bean/UserEntity.java
@@ -11,4 +11,51 @@ package ch16_bean;
     +----------------------------------------+
  */
 public class UserEntity {
+    private int username;
+    private int password;
+    private String email;
+    private String name;
+
+//    setter / getter / toString을 정의하세요
+    public int getUsername() {
+        return username;
+    }
+
+    public void setUsername(int username) {
+        this.username = username;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "username=" + username +
+                ", password=" + password +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
